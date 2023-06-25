adb2e65159efff55cd64baf524f6850e955fe65b
Shanto <shanto@Shantos-MacBook-Air.local>
2023-06-25 13:31:04 +0600

    config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+  // baseUrl: "https://naf.test-api.viserx.net/api",
+  // "baseUrl": 'https://inventory-part-backend.shohorabshanto.com/api',
+  baseUrl: "http://product.test/api", 
+}; 
+
+
